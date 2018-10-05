89a75673bffb8149edf4084d6c4651ce1cedea72
Dayna <daynajblackwell@gmail.com>
2018-10-05 12:51:16 -0700

    Building index.html.  Added support for sass stylesheets.  Building project folder directory.

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const app = express();
 const cors = require('cors');
 
 const log = msg => console.log(msg);
-const log = msg => console.log(msg);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/view', express.static(process.cwd() + '/view'));
+
+app.get('/', (req,res) => {
+
+});
+
+
+
+
+
+
+app.listen(port, () => console.log(`server started on port ${port}`));
