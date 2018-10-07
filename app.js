3fa3588e8fecf1dbaed4ff043fd32f50a7d7bad9
Dayna <daynajblackwell@gmail.com>
2018-10-07 05:33:56 -0700

    emergency thunderstorm commit

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const log = msg => console.log(msg);
-const routes = require('./routes/routes.js');
+// const routes = require('./routes/routes.js');
+
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/public', express.static(process.cwd() + '/view'));
+
+app.use(require('./routes/routes.js'))
+// app.use('/public', express.static(process.cwd() + '/view'));
 
 // log(routes);
 
-app.get('/', (req,res) => {
-    res.sendFile(process.cwd() + '/view/index.html');
-});
+// app.get('/', (req,res) => {
+//     res.sendFile(process.cwd() + '/view/index.html');
+// });
 
 app.listen(port, () => console.log(`server started on port ${port}`));
--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
 
 const log = msg => console.log(msg);
-
 let users = [];
 
+app.use('/public', express.static(process.cwd() + '/view'));
+
 router.get('/', (req,res) => {
-    app.use('/public', express.static(process.cwd() + '../view'));
-    res.sendFile(process.cwd() + '../view/index.html');
+    log(process.cwd());
+    res.sendFile(process.cwd() + '/view/index.html');
 });
 
 router.get('/api/exercise/users' , (req,res) => {
