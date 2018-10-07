b87ceefebe7f85931f5c9005a173ff939771d36e
Dayna <daynajblackwell@gmail.com>
2018-10-07 12:48:03 -0700

    Configuring app for heroku

--- routes/routes.js
+++ routes/routes.js
@@ -9,10 +9,11 @@ let users = ['a user'];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public', express.static(process.cwd() + '/view'));
+app.use('/public/', express.static(process.cwd() + '/view/'));
 
 router.get('/', (req,res) => {
-    log(process.cwd());
+    log(process.cwd())
+    log(__dirname);
     res.sendFile(process.cwd() + '/view/index.html');
 });
 
