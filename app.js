d1d9770ae3db6a467ea5e58c987f805392b17a28
Dayna <daynajblackwell@gmail.com>
2018-10-07 20:41:19 -0700

    Fixed error with serving static resources.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const sass = require('node-sass');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const log = msg => console.log(msg);
-// const routes = require('./routes/routes.js');
+
+const routes = require('./routes/routes.js');
 
 
 app.use(cors());
@@ -14,12 +14,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(require('./routes/routes.js'))
-// app.use('/public', express.static(process.cwd() + '/view'));
 
+app.use('/', express.static(process.cwd() + '/view'));
 // log(routes);
+app.get('/', (req,res) => {
+    res.sendFile(process.cwd() + '/view/index.html');
+});
 
-// app.get('/', (req,res) => {
-//     res.sendFile(process.cwd() + '/view/index.html');
-// });
+
 
 app.listen(port, () => console.log(`server started on port ${port}`));
--- routes/routes.js
+++ routes/routes.js
@@ -10,14 +10,6 @@ let users = ['a user'];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/public/', express.static(process.cwd() + '/view/'));
-
-router.get('/', (req,res) => {
-    log(process.cwd())
-    log(__dirname);
-    res.sendFile(process.cwd() + '/view/index.html');
-});
-
 router.get('/api/exercise/users' , (req,res) => {
     res.send(users);
 });
