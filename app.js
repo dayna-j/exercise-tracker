01e1dfcfa1ca3870a01fcba0a1b48f391f5882ac
Dayna <daynajblackwell@gmail.com>
2018-10-08 19:10:30 -0700

    Added mongoose.  Beginning to implement database.  Adding support for Model component of MVC architecture.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 // packages
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+const db = process.env.MONGO_URL;
+const mongoose = require('mongoose');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
-
 
 const port = process.env.PORT || 3000;
 const log = msg => console.log(msg);
-const db = process.env.MONGO_URL;
-log(db);
 
+// log(db);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
