64051578cb1fe2794f1caef9fbb4a77415d8acea
Dayna <daynajblackwell@gmail.com>
2018-10-08 18:23:40 -0700

    Added mongoose.  Added dotenv to support .env file in local development environemnt.

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
 
 const port = process.env.PORT || 3000;
 const log = msg => console.log(msg);
+const db = process.env.MONGO_URL;
+log(db);
 
 app.use(cors());
 app.use(bodyParser.json());
