4171604117d3c969957326f429accc4d7c369587
Dayna <daynajblackwell@gmail.com>
2018-10-13 20:19:21 -0700

    Fixing problems raised by eslint

--- model/User.js
+++ model/User.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
-const log = (msg) => console.log(msg);
 const shortId = require('shortid');
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    userId: {
-        type: String,
-        required: true,
-        default: shortId.generate
-    },
-    exercise: [{
-        description: String,
-        duration: Number,
-        date: String
-    }]
+  username: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+    default: shortId.generate,
+  },
+  exercise: [{
+    description: String,
+    duration: Number,
+    date: String,
+  }]
 });
 module.exports = User = mongoose.model('User', UserSchema);
